bcfb02066d73014ebf66d8be92cacc6d5ffda66f
ccraven9 <c.craven1337@gmail.com>
2024-01-21 22:48:55 -0500

    Adding reactive signup form to sign-up-page

--- src/app/pages/sign-up-page/sign-up-page.component.ts
+++ src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up-page',
@@ -7,11 +9,48 @@ import { Component } from '@angular/core';
 })
 export class SignUpPageComponent {
 
+
   displaySignupForm = false;
+  showEmailHelper = false;
+  signupFormEmail: FormGroup;
+  signupFormUserAndPass: FormGroup;
+  private userEmail: string | undefined;
+
+  constructor(private fb: FormBuilder) {
+    this.signupFormEmail = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]]
+      }
+    );
+
+    this.signupFormUserAndPass = this.fb.group(
+      {
+        username: ['', [Validators.required]],
+        passowrd: ['', [Validators.required]]
+      }
+    )
+  }
 
 
   showSignupForm() {
     this.displaySignupForm = true;
+  }
+
+  onSignupEmailSubmit() {
+    if (!this.signupFormEmail.invalid) {
+      const emailValue = this.signupFormEmail.get('email')?.value;
+      this.userEmail = emailValue;
+      console.log('Submitted email:', emailValue);
+      this.showSignupForm();
+    }
+
+    this.showEmailHelper = true;
+    return;
+
+  }
+
+  onSignupUserAndPassSubmit() {
+    
   }
 
 }
